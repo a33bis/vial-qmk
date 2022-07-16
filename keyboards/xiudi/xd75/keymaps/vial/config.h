27f04a403904312fefa546d19d48ce8a795db696
a33bis <yetanotheraccount@mailbox.org>
2022-07-16 11:22:00 -0400

    Added vial support. RGBLIGHT and BACKLIGHT disabled.

---
+++ keyboards/xiudi/xd75/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x24, 0xB0, 0xCF, 0x64, 0x7A, 0xC9, 0x82, 0x84}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 14 }
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
