a1556990605f6041a11638ed38258188050b8183
a33bis <yetanotheraccount@mailbox.org>
2023-06-24 19:55:33 -0400

    AKB OGRN Support

---
+++ keyboards/akb/ogrn/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x55, 0x72, 0xF4, 0x06, 0x45, 0x2D, 0x6C, 0x61}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
